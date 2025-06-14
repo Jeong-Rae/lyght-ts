53670a49eb813ba6bcaadaebc7a24e7927471a80
JRJR519 <kkwjdfo@gmail.com>
2025-06-14 23:47:47 +0900

    logger 패키지 초기 설정

---
+++ packages/logger/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src/index.ts"],
+	format: ["esm", "cjs"],
+	dts: true,
+	splitting: true,
+	sourcemap: true,
+	clean: true,
+	minify: false,
+	outDir: "dist",
+	target: "esnext",
+	skipNodeModulesBundle: true,
+});
---
+++ packages/logger/vitest.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "vitest/config";
+import packageJson from "./package.json";
+
+export default defineConfig({
+	test: {
+		name: packageJson.name,
+		exclude: ["./benchmarks/**/*", ".yarn/**/*"],
+		coverage: {
+			provider: "istanbul",
+			include: ["src/**/*"],
+			exclude: ["src/compat/_internal/**/*", "src/**/*.spec.ts"],
+		},
+		watch: false,
+		deps: {
+			optimizer: {
+				ssr: {
+					enabled: true,
+					include: ["es-toolkit", "es-toolkit/compat"],
+				},
+			},
+		},
+	},
+});
